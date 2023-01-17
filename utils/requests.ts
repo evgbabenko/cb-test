f82826829316b4d168592ac33a7808b699ed1344
Ievgen Babenko <evgbabenko@live.com>
2023-01-17 14:36:07 +0200

    api link changes

--- utils/requests.ts
+++ utils/requests.ts
@@ -1,7 +1,5 @@
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-/* const BASE_URL = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='; */
-
- const BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='; 
+ const BASE_URL = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='; 
 
 const request_news = `${BASE_URL}${API_KEY}`;
 
